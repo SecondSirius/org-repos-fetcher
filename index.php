3a34b8d207eb238f00f6f79887a9289bc3786ace
Szymon Bobowski <szymon.bobowski.7@gmail.com>
2022-03-22 13:09:06 +0100

    added fetch data function, and cacert.pem file for a peer verification

--- index.php
+++ index.php
@@ -26,7 +26,56 @@ function check_org() {
   }
 }
 
-echo check_org() ? $org : "ERROR";
+if (isset($_GET['auth_token']) && check_org()) {
+  $auth_token = $_GET['auth_token'];
+
+  $headers = [
+    "Authorization: Token $auth_token",
+    "User-Agent: PHP"
+  ];
+    
+  $options = [
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => $headers,
+    CURLOPT_SSL_VERIFYPEER => true,
+    CURLOPT_CAINFO => "cacert.pem"
+  ];
+
+  // sends a single request to URL
+  // returns response headers(as an array) and body(as a string)
+  function fetch_data(&$ch, $url, $opts) {
+    curl_setopt_array($ch, $opts);
+    curl_setopt($ch, CURLOPT_URL, $url);
+
+    // mapping headers into an associative array
+    $headers = [];
+    curl_setopt($ch, CURLOPT_HEADERFUNCTION, 
+      function ($curl, $header) use (&$headers) {
+        $len = strlen($header);
+        $header = explode(':', $header, 2);
+        if (count($header) < 2) return $len;
+        $headers[strtolower(trim($header[0]))] = trim($header[1]);
+        return $len;
+      }
+    );
+
+    $body = curl_exec($ch);
+    $res_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    if ($res_code >= 400) {
+      return $res_code;
+    }
+    return ["headers" => $headers, "body" => $body];
+  }
+
+  $ch = curl_init();
+  var_dump(fetch_data(
+    $ch, 
+    "https://api.github.com/orgs/$org/repos", 
+    $options
+  ));
+  curl_close($ch);
+}
 
 ?>
 
